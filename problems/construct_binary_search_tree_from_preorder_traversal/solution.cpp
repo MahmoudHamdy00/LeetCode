1c735587d5a1e7b5daf5a49b9f371766e379f529
Mahmoud Hamdy <mahmoudh.morsy@gmail.com>
2020-05-24 10:42:34 +0000

    Sync LeetCode submission - Construct Binary Search Tree from Preorder Traversal (cpp)

--- problems/construct_binary_search_tree_from_preorder_traversal/solution.cpp
+++ problems/construct_binary_search_tree_from_preorder_traversal/solution.cpp
@@ -4,59 +4,33 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<int> pre;
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        pre=preorder;
-        int preIndex = 0;  
-        return constructTreeUtil ( &preIndex, 0, pre.size() - 1);  
+        TreeNode* root=NULL;
+        for(int i:preorder){
+            root=insert(root,i);
+        }
+        return root;
     }
-   TreeNode* newNode (int data)  
-{  
-    TreeNode* temp = new TreeNode(); 
-  
-    temp->val = data;  
-    temp->left = temp->right = NULL;  
-  
-    return temp;  
-}  
-  
-// A recursive function to construct Full from pre[]. preIndex is used  
-// to keep track of index in pre[].  
-TreeNode* constructTreeUtil ( int* preIndex, int low, int high)  
-{  
-    // Base case  
-    if (*preIndex >= pre.size() || low > high)  
-        return NULL;  
-  
-    // The first node in preorder traversal is root. So take the node at  
-    // preIndex from pre[] and make it root, and increment preIndex  
-    TreeNode* root = newNode ( pre[*preIndex] );  
-    *preIndex = *preIndex + 1;  
-  
-    // If the current subarry has only one element, no need to recur  
-    if (low == high)  
-        return root;  
-  
-    // Search for the first element greater than root  
-    int i;  
-    for ( i = low; i <= high; ++i )  
-        if ( pre[ i ] > root->val )  
-            break;  
-  
-    // Use the index of element found in preorder to divide  
-        // preorder array in two parts. Left subtree and right subtree  
-    root->left = constructTreeUtil ( preIndex, *preIndex, i - 1 );  
-    root->right = constructTreeUtil (  preIndex, i, high );  
-  
-    return root;  
-}  
-  
-
-  
-
+    TreeNode* insert(TreeNode* node,int val){
+        TreeNode* nw=new TreeNode();
+        nw->val=val;
+        nw->left=NULL;
+        nw->right=NULL;
+        if(node==NULL){
+            
+            return node=nw;;
+        }
+        if(node->val>val){
+            node->left= insert(node->left,val);
+        }
+        else node->right =insert(node->right,val);
+        return node;
+   }
 };

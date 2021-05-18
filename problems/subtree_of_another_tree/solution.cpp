92652bb2225b4168e412802a299604c2ec65cd8c
Mahmoud Hamdy <mahmoudh.morsy@gmail.com>
2021-05-18 13:08:14 +0000

    Sync LeetCode submission - Subtree of Another Tree (cpp)

--- problems/subtree_of_another_tree/solution.cpp
+++ problems/subtree_of_another_tree/solution.cpp
@@ -14,18 +14,24 @@ public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         if(subRoot==nullptr)return 1;
         if(root==nullptr)return 0;
-        bool ret=0;
-        if(isIdentical(root,subRoot))
-           return true;
-        ret|=isSubtree( root->right,  subRoot);
-        ret|=isSubtree( root->left,  subRoot);
-        return ret;
+        string s,k;
+        get(root,s);
+        get(subRoot,k);
+        int idx=s.find(k);
+        while(idx!=-1){
+            if(idx==0)return true;
+            if(s[idx-1]==' ')return true;
+            idx=s.find(k,idx+1);
+        }
+        return false;
     }
-    bool isIdentical(TreeNode* root, TreeNode* subRoot) {
-        if(root==nullptr&&subRoot==nullptr)return true;
-        if(root==nullptr||subRoot==nullptr)return false;
-        if(root->val!=subRoot->val)return false;
-        return isIdentical(root->left,subRoot->left)&isIdentical(root->right,subRoot->right);
-
+    void get(TreeNode* root,string&s) {
+        if(root==nullptr){
+            s+="X ";
+            return ;
+        }
+        get(root->left,s);
+        get(root->right,s);
+        s+=to_string(root->val)+" ";
     }
 };

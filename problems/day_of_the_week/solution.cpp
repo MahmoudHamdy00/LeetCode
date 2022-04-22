c19f5cd0f0d2cd099c78cceb156485c6e28918ac
Mahmoud Hamdy <mahmoudh.morsy@gmail.com>
2022-04-22 07:47:37 +0000

    Sync LeetCode submission - Day of the Week (cpp)

---
+++ problems/day_of_the_week/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string dayOfTheWeek(int day, int month, int year) {
+        vector<string> days= {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        return days[dayofweek(day,  month,  year)];
+    }
+    int dayofweek(int d, int m, int y)
+    {
+        static int t[] = { 0, 3, 2, 5, 0, 3,
+                           5, 1, 4, 6, 2, 4 };
+        y -= m < 3;
+        return ( y + y / 4 - y / 100 +
+                 y / 400 + t[m - 1] + d) % 7;
+    }
+ 
+};

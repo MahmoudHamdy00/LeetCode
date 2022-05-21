b1fd03f47b502f1b145e89256bae76f84395f4f7
Mahmoud Hamdy <mahmoudh.morsy@gmail.com>
2022-05-21 17:04:41 +0200

    Time: 49 ms (95.52%), Space: 14.4 MB (68.59%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>mem(1e4,-1);
-        int ret=coinChange(coins,amount,mem);
-        return ret==1e9?-1:ret;
-    }
-    int coinChange(vector<int>& coins, int amount,vector<int>&mem) {
-        if(amount==0)return 0;
-        if(amount <0)return 1e9;
-        int &ret=mem[amount];
-        if(~ret)return ret;
-        ret=1e9;
-        for(int i:coins){
-            ret=min(ret,coinChange(coins,amount-i,mem)+1);
+        vector<int>mem(amount+1,1e9);
+        mem[0]=0;
+        for(int am=0;am<=amount;++am){
+            int&ret=mem[am];
+            for(int i:coins){
+                if(am-i>=0)
+                    ret=min(ret,mem[am-i]+1);
+            }
         }
-        return ret;
+        return mem[amount]==1e9?-1:mem[amount];
     }
 };

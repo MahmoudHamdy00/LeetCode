1018f4d23ac2d335c37289f457d04b72832ae7bc
Mahmoud Hamdy <mahmoudh.morsy@gmail.com>
2022-05-30 22:53:24 +0000

    Sync LeetCode submission - Divide Two Integers (cpp)

--- problems/divide_two_integers/solution.cpp
+++ problems/divide_two_integers/solution.cpp
@@ -3,7 +3,21 @@ class Solution
 public:
 	int divide(int dividend, int divisor)
 	{
-        long long ret=1LL*dividend/divisor;
-		return (ret>INT_MAX?INT_MAX:ret);
+		long long q = 0, t = 0,a=dividend,b=divisor;
+		int s = 1;
+		if ((a < 0 && b > 0) || (a > 0 && b < 0))
+			s = -1;
+		a = abs(a), b = abs(b);
+		for (int i = 31; i >= 0; i--)
+		{
+			if ((t + (b << i)) <= a)
+			{
+				t += b << i;
+				q |= 1LL << i;
+			}
+		}
+		if ((s * q) > INT_MAX)
+			return INT_MAX;
+		return s * q;
 	}
 };

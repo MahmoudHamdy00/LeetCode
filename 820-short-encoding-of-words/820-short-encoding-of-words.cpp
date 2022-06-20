460fd4a0c4546fb225629f8946ce512c41cfe304
Mahmoud Hamdy <mahmoudh.morsy@gmail.com>
2022-06-20 08:12:52 +0200

    Time: 165 ms (29.71%), Space: 18.3 MB (69.31%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumLengthEncoding(vector<string>& words) {
+        if(words.size()==0)return 0;
+        int cnt=0;
+        set<string>st;
+        for(string& s:words)
+            st.insert(s);
+        for(string& s:words)
+            for(int i=1;i<s.size();++i){
+                st.erase(s.substr(i));
+            }
+        for(auto s:st)
+            cnt+=s.size()+1;
+        
+        return cnt;
+    }
+};

f4d6508d4a4537b96efec30f4db5d44797908c7b
Mahmoud Hamdy <mahmoudh.morsy@gmail.com>
2022-06-26 06:13:12 +0200

    Time: 127 ms (25.00%), Space: 6.9 MB (50.00%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -12,17 +12,17 @@ public:
         int &ret=mem[i][prv];
         if(~ret)return ret;
         ret=0;
-        ret+=solve(i+1,0,n),
-        ret%=mod;
+            ret+=solve(i+1,0,n),
+            ret%=mod;
         if((prv&1)==0)
-        ret+=solve(i+1,1,n),
-        ret%=mod;
+            ret+=solve(i+1,1,n),
+            ret%=mod;
         if((prv&2)==0)
-        ret+=solve(i+1,2,n),
-        ret%=mod;
+            ret+=solve(i+1,2,n),
+            ret%=mod;
         if((prv&3)==0)
-        ret+=solve(i+1,3,n),
-        ret%=mod;
+            ret+=solve(i+1,3,n),
+            ret%=mod;
         return ret%=mod;
     }
 };

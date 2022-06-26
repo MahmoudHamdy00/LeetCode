440ef1b3727377b05356e5886ec3d0ca32dc396f
Mahmoud Hamdy <mahmoudh.morsy@gmail.com>
2022-06-26 06:04:49 +0200

    Time: 91 ms (25.00%), Space: 6.8 MB (50.00%) - LeetHub

---
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int mem[10005][4],mod=1e9+7;
+public:
+    int countHousePlacements(int n) {
+        memset(mem,-1,sizeof mem);
+        return solve(0,0,n);
+    }
+    int solve(int i,int prv,int n) {
+        if(i==n){
+            return 1;
+        }
+        int &ret=mem[i][prv];
+        if(~ret)return ret;
+        ret=0;
+        ret+=solve(i+1,0,n),
+        ret%=mod;
+        if((prv&1)==0)
+        ret+=solve(i+1,1,n),
+        ret%=mod;
+        if((prv&2)==0)
+        ret+=solve(i+1,2,n),
+        ret%=mod;
+        if((prv&3)==0)
+        ret+=solve(i+1,3,n),
+        ret%=mod;
+        return ret%=mod;
+    }
+};

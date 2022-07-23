d850bf854e62c98802152eb942f5100d93f5704e
Mahmoud Hamdy <mahmoudh.morsy@gmail.com>
2022-07-23 20:04:34 +0200

    Time: 390 ms (100.00%), Space: 108.4 MB (100.00%) - LeetHub

--- 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.cpp
+++ 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    int shortestSequence(vector<int>& rolls, int k) {
-        return solve(0,1,rolls,k);
-    }
-    int solve(int i,int step,vector<int>& rolls, int k){
-        if(i==rolls.size())return step;
-        set<int>st;
-        int idx=i;
-        for(int j=i;j<rolls.size();++j){
-            st.insert(rolls[j]);
-            if(st.size()==k){
-                idx=j;
-                break;
+    int shortestSequence(vector<int>& A, int k) {
+        int res = 1;
+        unordered_set<int> s;
+        for (int& a : A) {
+            s.insert(a);
+            if (s.size() == k) {
+                res++;
+                s.clear();
             }
         }
-        int ret=1e9;
-        if(st.size()!=k)return step;
-        return solve(idx+1,step+1,rolls,k);
+        return res;
     }
 };

81a4ea1be237f7f1fa14e91ef2a7d817d1f763f8
Mahmoud Hamdy <mahmoudh.morsy@gmail.com>
2022-07-31 01:53:44 +0200

    Time: 218 ms (82.38%), Space: 62.8 MB (80.84%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        map<char,int>mp;
+        int mp[26]={0};
         for(string &s:words2){
-            map<char,int>cur;
+            int cur[26]={0};
             for(char &ch:s)
-                ++cur[ch];
-            for(auto it:cur){
-                mp[it.first]=max(it.second,mp[it.first]);            
-                //cout<<it.first<<" "<<it.second<<" "<<cur[it.first]<<endl;
+                ++cur[ch-'a'];
+            for(int i=0;i<26;++i){
+                mp[i]=max(cur[i],mp[i]);            
             }
         }
-
         vector<string>v;
         set<string>st;
         for(string &s:words1){
             if(st.count(s))continue;
-            map<char,int>cur;
+            int cur[26]={0};
             for(char &ch:s)
-                ++cur[ch];
+                ++cur[ch-'a'];
             bool ok=1;
-            for(auto it:mp){
-                 //cout<<it.first<<" "<<it.second<<" "<<cur[it.first]<<endl;
-                if(cur[it.first]<it.second){
+            for(int i=0;i<26;++i){
+                if(cur[i]<mp[i]){
                     ok=0;
                     break;
-                }    
+                }              
+               
             }
-            //cout<<"FF"<<v.size()<<endl;
-
             if(ok){
-//cout<<s<<endl;
                 v.push_back(s);
                 st.insert(s);
             }

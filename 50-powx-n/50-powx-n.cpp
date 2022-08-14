f62965d9b75348f14a2f926be099b7b4c36622f7
Mahmoud Hamdy <mahmoudh.morsy@gmail.com>
2022-08-14 11:19:04 +0200

    Time: 0 ms (100.00%), Space: 5.8 MB (96.18%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        double p=1;
+        bool negative=n<0;
+        long long nn=1LL*abs(n);
+        p=poww(x,nn);
+        if(negative)p=1/p;
+        return p;
+    }
+    double poww(double x, long long n) {
+        if(n==0)return 1;
+        if(n==1)return x;
+        double ret=poww(x,n>>1);
+        ret*=ret;
+        if(n&1)ret*=x;
+        return ret;
     }
 };

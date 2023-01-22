8bb75b60d99929aa4c387e602e8e78d552fb5034
Mahmoud Hamdy <mahmoudh.morsy@gmail.com>
2023-01-22 13:32:57 +0200

    Time: 126 ms (80.21%), Space: 75.8 MB (34.66%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -3,7 +3,7 @@ class Solution {
     vector<string>v;
 public:
     vector<vector<string>> partition(string s) {
-        solve(0,0,s);
+        solve(0,s);
         return ans;
     }
     bool isPal(int l,int r,string &s){
@@ -13,18 +13,17 @@ public:
         }
         return 1;
     }
-    void solve(int l,int r,string &s){
-        if(r==s.size()){
-            if(l==r)ans.push_back(v);
+    void solve(int l,string &s){
+        if(l==s.size()){
+            ans.push_back(v);
             return;
         }
-       
-        if(isPal(l,r,s)){
-            v.push_back(s.substr(l,r-l+1));
-            solve(r+1,r+1,s);
-            v.pop_back();
+        for(int r=l;r<s.size();++r){
+            if(isPal(l,r,s)){
+                v.push_back(s.substr(l,r-l+1));
+                solve(r+1,s);
+                v.pop_back();
+            }
         }
-                
-        solve(l,r+1,s);
     }
 };
